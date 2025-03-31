4a7f9d9f3d9aa7136f98d3b6d09b084d15c86451
= <=>
2025-03-31 18:38:04 +0300

    heap sort visualizes a binary tree and not actually creating a tree

--- node.py
+++
@@ -1,9 +0,0 @@
-class Node:
-    def __init__(self, key)-> None:
-        self.key = key
-        self.left = None
-        self.right = None
-    def __str__(self)-> str:
-        return f"{self.key}"
-    def __repr__(self):
-        return self.__str__()
