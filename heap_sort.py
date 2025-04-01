c216c3ff1623810dbe130450a5c3a63d57e00ebd
= <=>
2025-04-01 20:12:50 +0300

    reduce the number of loops as you go up the tree

--- heap_sort.py
+++ heap_sort.py
@@ -3,23 +3,30 @@ from math import floor
 
 def max_heap(numbers: List[int]) -> List[int]:
 
-    current_index = floor(len(numbers) / 2) - 1
+    current_index = floor(len(numbers) / 2)
+    times = (len(numbers) - current_index)
     while current_index > 0:
-        print(f"Index of first first leaf: {current_index}")
-        for index in range(current_index, len(numbers) - 1):
+        print(f"Index of current: {current_index}")
+        for index in range(current_index, times + 1):
 
             parent = floor(index - 1 / 2)
             left = 2 * parent + 1
             right = 2 * parent + 2
 
             if left < len(numbers):
-                if left > 0 and right > 0:
-                    print(f"Before swap  Parent: {numbers[parent]}, Left: {numbers[left]} Right: {numbers[right]}")
-                if numbers[parent] < numbers[left]:
-                    numbers[parent], numbers[left] = numbers[left], numbers[parent]
+                if left > 0 :
+                    print(f"Before swap on the left Parent: {numbers[parent]}, Left: {numbers[left]}")
+                    if numbers[parent] < numbers[left]:
+                        numbers[parent], numbers[left] = numbers[left], numbers[parent]
+                        print(f"After swap on the left Parent: {numbers[parent]}, Left: {numbers[left]}")
+                    print()
             if right < len(numbers):
-                if numbers[parent] < numbers[right]:
-                    numbers[parent], numbers[right] = numbers[right], numbers[parent]
-                if left > 0 and right > 0:
-                    print(f"After swap  Parent: {numbers[parent]}, Left: {numbers[left]} Right: {numbers[right]}")
+                if right > 0:
+                    print(f"Before swap on the right ,  Parent: {numbers[parent]}, Right: {numbers[right]}")
+                    if numbers[parent] < numbers[right]:
+                        numbers[parent], numbers[right] = numbers[right], numbers[parent]
+                        print(f"After swap on the right ,  Parent: {numbers[parent]}, Right: {numbers[right]}")
+                    print()
+
         current_index -= 1
+        times = times // 2
